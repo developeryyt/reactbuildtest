99d8bbe1bc574d899890a32ede873dba69b729b2
developeryyt <yeontaey92@gmail.com>
2023-11-29 13:50:29 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+// import Cart from '../src/assets/imgs/icon_cart.png';
+import '../src/assets/style/main.scss'
+
+
+
+
+function App() {
+
+  function onClick() {
+
+  }
+
+
+  return (
+    <div className='app'>
+      <button type='button' className='btn' onClick={onClick}>
+        버튼
+      </button>
+    </div>
+  )
+}
+
+export default App
